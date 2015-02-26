512736dfda828809f7ef6b2d354be1c36fae8005
Don Abrams <don.abrams@sympoz.com>
2015-02-26 10:12:12 -0700

    add ability to export this for most js environments, shamelessly taken from lodash

--- arrg.js
+++ arrg.js
@@ -1,4 +1,6 @@
-module.exports = Arrg;
+
+//Thank craftsy
+;(function() {
 
 Arrg.req = Arrg.required = {};
 function Arrg(func, args, allowMutable) {
@@ -58,6 +60,69 @@ function deepFreeze(o) {
 
 function deepCopy(o) {
 	//TODO: does not handle assigning correct Object.prototype
-	//fastest method as per http://web.archive.org/web/20140328224025/http://jsperf.com/cloning-an-object/2
+	//Fastest method as per http://web.archive.org/web/20140328224025/http://jsperf.com/cloning-an-object/2
 	return JSON.parse(JSON.stringify(o));
 }
+
+
+/* Crazy part so I can export this for most build/js environments 
+	including io, amd, browser, etc */
+
+  /** Used to determine if values are of the language type `Object`. */
+  var objectTypes = {
+    'function': true,
+    'object': true
+  };
+  /**
+   * Used as a reference to the global object.
+   *
+   * The `this` value is used if it is the global object to avoid Greasemonkey's
+   * restricted `window` object, otherwise the `window` object is used.
+   */
+  var root = (objectTypes[typeof window] && window !== (this && this.window)) ? window : this;
+
+  /** Detect free variable `exports`. */
+  var freeExports = objectTypes[typeof exports] && exports && !exports.nodeType && exports;
+
+  /** Detect free variable `module`. */
+  var freeModule = objectTypes[typeof module] && module && !module.nodeType && module;
+
+  /** Detect free variable `global` from Node.js or Browserified code and use it as `root`. */
+  var freeGlobal = freeExports && freeModule && typeof global == 'object' && global;
+  if (freeGlobal && (freeGlobal.global === freeGlobal || freeGlobal.window === freeGlobal || freeGlobal.self === freeGlobal)) {
+    root = freeGlobal;
+  }
+
+  /** Detect the popular CommonJS extension `module.exports`. */
+  var moduleExports = freeModule && freeModule.exports === freeExports && freeExports;
+
+  // Some AMD build optimizers like r.js check for condition patterns like the following:
+  if (typeof define == 'function' && typeof define.amd == 'object' && define.amd) {
+    // Expose Arrg to the global object when an AMD loader is present to avoid
+    // errors in cases where Arrg is loaded by a script tag and not intended
+    // as an AMD module. See http://requirejs.org/docs/errors.html#mismatch for
+    // more details.
+    root.__ = Arrg;
+
+    // Define as an anonymous module so, through path mapping, it can be
+    // referenced as the "arrg" module.
+    define(function() {
+      return Arrg;
+    });
+  }
+  // Check for `exports` after `define` in case a build optimizer adds an `exports` object.
+  else if (freeExports && freeModule) {
+    // Export for Node.js or RingoJS.
+    if (moduleExports) {
+      (freeModule.exports = Arrg).__ = Arrg;
+    }
+    // Export for Narwhal or Rhino -require.
+    else {
+      freeExports.__ = Arg;
+    }
+  }
+  else {
+    // Export for a browser or Rhino.
+    root.__ = Arrg;
+  }
+}.call(this));
